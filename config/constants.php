2f3bf2f5aa3ad49593671640b92cbca2cab1ea8f
Udesh Regmi <udeshregmi00@gmail.com>
2026-03-11 08:34:46 +0545

    Push with change in constant

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 // config/constants.php
 define('BASE_URL', 'http://localhost/BusReservation');
-define('SITE_NAME', 'Dhading Bus Sewa');
+define('SITE_NAME', 'Nepal Bus Sewa');
 define('ITEMS_PER_PAGE', 10);
 define('HOME_BANNER_IMG_URL','http://localhost/BusReservation/UI/gramhin-banner.jpeg');
 ?>
